3ea037dc29b4593b531bc20df792dc8dae63ae7e
KarenMorales16 <karen.morales16@tectijuana.edu.mx>
2020-02-28 16:37:30 -0800

    10 more scala functions

--- Unidad1/Practicas_Tarea/Practica6/Ejercicio1.scala
+++ Unidad1/Practicas_Tarea/Practica6/Ejercicio1.scala
@@ -36,3 +36,23 @@ df.select(skewness("Sales")).show()
 // 10 returns the population variance of the values in a group.
 df.select(var_pop("Sales")).show()
 
+// 11 returns the unbiased variance of the values in a group.
+df.select(var_samp("Sales")).show()
+//12 returns the minimum value of the column in a group.
+df.select(min("Sales")).show()
+//13 returns the maximum value of the column in a group.
+df.select(max("Sales")).show()
+//14 returns the sum of all values in the given column.
+df.select(sum("Sales")).show()
+//15 returns the sum of distinct values in the expression.
+df.select(sumDistinct("Sales")).show()
+//16 returns the average of the values in a group.
+df.select(mean("Sales")).show()
+//17 returns the sample standard deviation of the expression in a group.
+df.select(stddev_samp("Sales")).show()
+//18 Returns the value of the column e rounded to 0 decimal places with HALF_UP round mode.
+df.select(round(sum("Sales"))).show()
+//19 Computes the absolute value of a numeric value.
+df.select(abs(sum("Sales"))).show()
+//20. returns a set of objects with duplicate elements eliminated.
+df.select(collect_set("Person")).show()
