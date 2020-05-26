0ef42938b86c9df2a46918ce352e36a8e37f2f5b
Eduardoitt <jesus.escobedo16@tectijuana.edu.mx>
2020-05-25 21:42:03 -0700

    readme pracetice 4

--- Unidad2/Practicas_Tareas/Practica4/RandomForestClassifierExample.scala
+++ Unidad2/Practicas_Tareas/Practica4/RandomForestClassifierExample.scala
@@ -1,54 +1,50 @@
-
-
-// scalastyle:off println
+// We import libraries
 package org.apache.spark.examples.ml
-
-// $example on$
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.classification.{RandomForestClassificationModel, RandomForestClassifier}
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
-// $example off$
 import org.apache.spark.sql.SparkSession
 
-    val spark = SparkSession.builder.appName("RandomForestClassifierExample").getOrCreate()
+// we create the spark session variable
+val spark = SparkSession.builder.appName("RandomForestClassifierExample").getOrCreate()
 
-    // $example on$
-    // Load and parse the data file, converting it to a DataFrame.
-    val data = spark.read.format("libsvm").load("/home/eduardo/Escritorio/semestre_9/BigData/Unidad2/Practicas_Tareas/Data/sample_libsvm_data.txt")
+// We load the dataframe
+val data = spark.read.format("libsvm").load("/home/eduardo/Escritorio/semestre_9/BigData/Unidad2/Practicas_Tareas/Data/sample_libsvm_data.txt")
 
-    // Index labels, adding metadata to the label column.
-    // Fit on whole dataset to include all labels in index.
-    val labelIndexer = new StringIndexer().setInputCol("label").setOutputCol("indexedLabel").fit(data)
-    // Automatically identify categorical features, and index them.
-    // Set maxCategories so features with > 4 distinct values are treated as continuous.
-    val featureIndexer = new VectorIndexer().setInputCol("features").setOutputCol("indexedFeatures").setMaxCategories(4).fit(data)
+// Index labels, adding metadata to the label column.
+// We make an adjustment to our dataframe
+val labelIndexer = new StringIndexer().setInputCol("label").setOutputCol("indexedLabel").fit(data)
 
-    // Split the data into training and test sets (30% held out for testing).
-    val Array(trainingData, testData) = data.randomSplit(Array(0.7, 0.3))
+// We add the characteristics to which we will adjust the dataframe
+val featureIndexer = new VectorIndexer().setInputCol("features").setOutputCol("indexedFeatures").setMaxCategories(4).fit(data)
 
-    // Train a RandomForest model.
-    val rf = new RandomForestClassifier().setLabelCol("indexedLabel").setFeaturesCol("indexedFeatures").setNumTrees(10)
+// We divide the dataframe into 70% for training and 30% for testing
+val Array(trainingData, testData) = data.randomSplit(Array(0.7, 0.3))
 
-    // Convert indexed labels back to original labels.
-    val labelConverter = new IndexToString().setInputCol("prediction").setOutputCol("predictedLabel").setLabels(labelIndexer.labels)
+// Train a RandomForest model.
+val rf = new RandomForestClassifier().setLabelCol("indexedLabel").setFeaturesCol("indexedFeatures").setNumTrees(10)
 
-    // Chain indexers and forest in a Pipeline.
-    val pipeline = new Pipeline().setStages(Array(labelIndexer, featureIndexer, rf, labelConverter))
+// Convert indexed labels back to original labels.
+val labelConverter = new IndexToString().setInputCol("prediction").setOutputCol("predictedLabel").setLabels(labelIndexer.labels)
 
-    // Train model. This also runs the indexers.
-    val model = pipeline.fit(trainingData)
+// we create a pipeline with pipeline
+val pipeline = new Pipeline().setStages(Array(labelIndexer, featureIndexer, rf, labelConverter))
 
-    // Make predictions.
-    val predictions = model.transform(testData)
+// We train the model
+val model = pipeline.fit(trainingData)
 
-    // Select example rows to display.
-    predictions.select("predictedLabel", "label", "features").show(5)
+// We transform predictions
+val predictions = model.transform(testData)
 
-    // Select (prediction, true label) and compute test error.
-    val evaluator = new MulticlassClassificationEvaluator().setLabelCol("indexedLabel").setPredictionCol("prediction").setMetricName("accuracy")
-    val accuracy = evaluator.evaluate(predictions)
-    println(s"Test Error = ${(1.0 - accuracy)}")
+// Select example rows to display.
+predictions.select("predictedLabel", "label", "features").show(5)
 
-    val rfModel = model.stages(2).asInstanceOf[RandomForestClassificationModel]
-    println(s"Learned classification forest model:\n ${rfModel.toDebugString}")
+// We select the labels that will be shown in the prediction
+val evaluator = new MulticlassClassificationEvaluator().setLabelCol("indexedLabel").setPredictionCol("prediction").setMetricName("accuracy")
+val accuracy = evaluator.evaluate(predictions)
+// We print the model accuracy
+println(s"Test Error = ${(1.0 - accuracy)}")
+// we print the model
+val rfModel = model.stages(2).asInstanceOf[RandomForestClassificationModel]
+println(s"Learned classification forest model:\n ${rfModel.toDebugString}")
